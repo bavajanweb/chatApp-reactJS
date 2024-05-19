bade4b1d3b6e640b7b33c1d152eeb1e80f3ea746
bavajan <wudanfusion@gmail.com>
2024-05-19 12:46:17 +0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import Chat from './components/chat/chat';
 import Detail from './components/detail/detail';
 import List from './components/list/list';
 import Login from './components/login/login';
 import Notification from './components/notification/notification';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './library/firebase';
 
 
 function App() {
 
   const user = false
+
+useEffect(()=>{
+  const unSub = onAuthStateChanged(auth,(user)=>{
+    console.log(user)
+  })
+  return () =>{
+    unSub()
+  }
+},[])
 
   return (
     <div className="container">
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import './login.css'
 import { toast } from 'react-toastify'
-import {createUserWithEmailAndPassword} from 'firebase/auth'
-import {auth, db} from '../../library/firebase'
-import {doc, setDoc} from 'firebase/firestore'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth'
+import { auth, db } from '../../library/firebase'
+import { doc, setDoc } from 'firebase/firestore'
+import Upload from '../../library/upload'
 
 
 
@@ -16,6 +17,8 @@ const Login = () => {
         url: ""
     })
 
+    const [loading, setLoading] = useState(false)
+
     const handAvatar = e => {
         if (e.target.files[0]) {
             setAvatar({
@@ -27,45 +30,71 @@ const Login = () => {
     }
 
     const handleRegister = async (e) => {
-        
+
         e.preventDefault()
+        setLoading(true)
         const formData = new FormData(e.target)
         const { username, email, password } = Object.fromEntries(formData)
         try {
 
-            const res = await createUserWithEmailAndPassword(auth,email,password)
+            const res = await createUserWithEmailAndPassword(auth, email, password)
 
-            await setDoc(doc(db, "users", res.user.uid),{
+            const imgUrl = await Upload(avatar.file)
+
+            await setDoc(doc(db, "users", res.user.uid), {
                 username,
                 email,
+                avatar: imgUrl,
                 id: res.user.uid,
                 blocked: [],
 
             })
+            await setDoc(doc(db, "userchats", res.user.uid), {
+                chats: [],
+
+            })
 
             toast.success("Account Created! you can login now")
-            
+
         } catch (err) {
             console.log(err)
             toast.error(err.message)
-            
+
+        }finally{
+            setLoading(false)
         }
-        
+
     }
 
 
 
-    const handleLogin = e =>{
+    const handleLogin = async(e) => {
         e.preventDefault()
+        setLoading(true)
+
+        const formData = new FormData(e.target)
+        const {email, password} =Object.fromEntries(formData)
+        try {
+
+            const res = await signInWithEmailAndPassword(auth,email, password) 
+            
+        } catch (err) {
+            console.log(err)
+            toast.error(err.message)
+
+            
+        }finally{
+            setLoading(false)
+        }
     }
     return (
         <div className="login">
             <div className="item">
                 <h2>Welcome back,</h2>
-                <form >
+                <form onSubmit={handleLogin}>
                     <input type='text' placeholder='Email' name='email' />
                     <input type='text' placeholder='password' name='password' />
-                    <button>Sign In</button>
+                    <button disabled={loading}>{loading ? "loading" : "Sign In"}</button>
                 </form>
             </div>
             <div className="seprator"></div>
@@ -78,7 +107,7 @@ const Login = () => {
                     <input type='text' placeholder='Username' name='username' />
                     <input type='text' placeholder='Email' name='email' />
                     <input type='text' placeholder='password' name='password' />
-                    <button>Sign Up</button>
+                    <button disabled={loading}>{loading ? "loading" : "Sign Up"}</button>
                 </form>
             </div>
 
